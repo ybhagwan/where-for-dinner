190d1399726a3fc8f0e2f28db82326c3c5f95885
Bhagwan Yadav <ybhagwan@vmware.com>
2023-08-07 18:15:15 +0530

    Added all where-for-dinner app files

---
+++ where-for-dinner-search-proc/src/main/java/com/java/example/tanzu/wherefordinner/model/SearchCriteria.java
@@ -0,0 +1,25 @@
+package com.java.example.tanzu.wherefordinner.model;
+
+import lombok.Data;
+
+@Data
+public class SearchCriteria 
+{
+	private String name;	
+	
+	private long startTime;
+	
+	private long endTime;
+	
+	private String diningTypes;
+	
+	private String diningNames;
+	
+	private String postalCode;
+	
+	private int radius;
+	
+	private String requestSubject;
+	
+	private String sendResultsTo;	
+}
